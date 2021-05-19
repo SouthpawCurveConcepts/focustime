8dace84214df258a4fe0876e89c3fcd160b1c74a
Jay For Short <jagoblacksky@gmail.com>
2021-05-19 01:39:17 -0500

    A few cleanup changes

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const STATUSES = {
 };
 
 export default function App() {
-  console.log('yolo')
+  console.log('yolo');
   const [focusSubject, setFocusSubject] = useState(null);
   const [focusHistory, setFocusHistory] = useState([]);
 
